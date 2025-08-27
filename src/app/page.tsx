15ada98500f1801295e6abc4f93da0151a776e2f
Prashant Sharma <117459774+prashxant@users.noreply.github.com>
2025-08-27 13:44:13 +0530

    gf bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,7 +131,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-b from-white via-blue-50 to-white font-mono">
+      <div className="min-h-screen [background:radial-gradient(125%_100%_at_50%_0%,_#FFF_6.32%,_#E0F0FF_29.28%,_#E7EFFD_68.68%,_#FFF_100%)] font-mono">
         <Header />
         <main className="max-w-4xl mx-auto px-8 text-center">
           <Solid />

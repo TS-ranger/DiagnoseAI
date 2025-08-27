a8214f4250a300c7416db327c3e408d6e9e7f70c
prashxant <notxdead0@gmail.com>
2025-08-27 19:03:20 +0530

    ui changes

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,7 +4,7 @@ export const PrimaryButton = ({ children, onClick, disabled }: { children: React
     <button
       onClick={onClick}
       disabled={disabled}
-      className="bg-black text-white px-4 py-2 rounded-full text-lg hover:bg-gray-800 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
+      className="bg-black text-white md:px-4 md:py-2  p-2 rounded-full text-lg hover:bg-gray-800 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
     >
       {children}
     </button>
